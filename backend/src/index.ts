1ec908c5ef716f82482a0a4fd8c456ef5bb48f26
Carol <carolinevictoria1224@gmail.com>
2026-06-11 11:10:55 -0300

    feat: criação da rota de cadastro de novo cliente

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -52,3 +52,23 @@ app.get("/api/clientes", async (req, res) => {
         res.status(500).json({ error: "Erro ao listar clientes" });
     }
 });
+
+// Rota para criar um novo cliente
+app.post("/api/clientes", async (req, res) => {
+    try {
+        const { nome, email, whatsapp, tipoDocumento, numeroDocumento } = req.body;
+
+        if (!nome || !email || !whatsapp || !tipoDocumento || !numeroDocumento) {
+            return res.status(400).json({ error: "Todos os campos são obrigatórios" });
+        }
+        
+        const novoCliente = await prisma.cliente.create({
+            data: { nome, email, whatsapp, tipoDocumento, numeroDocumento, },
+            include: { enderecos: true },
+        });
+        res.status(201).json(novoCliente);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro ao criar cliente" });
+    }
+});
